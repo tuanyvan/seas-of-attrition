f79462a8c3e10a3dde1287934c10d9d92e49a99a
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-01-12 22:35:09 -0500

    Created Random.java
    Random.java takes the random number generation methods in other classes and places them into one location as public static methods.

---
+++ src/com/tuany/Random.java
@@ -0,0 +1,20 @@
+package com.tuany;
+
+/**
+ * Contains static functions for random number generation.
+ * @author Tuany Van
+ * @version 1.0, 01/12/22
+ */
+
+public class Random {
+    // Declare private function that returns a random number within an inclusive range.
+    public static int getRandomInt(int lowerBound, int upperBound) {
+        return (int)(Math.random() * (upperBound + 1 - lowerBound) + lowerBound);
+    }
+
+    // Declare an overloaded private function that returns a random number from 0 to an inclusive upper bound.
+    public static int getRandomInt(int upperBound) {
+        return (int)(Math.random() * (upperBound + 1) + 0);
+    }
+
+}
